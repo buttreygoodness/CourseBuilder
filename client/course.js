126037854200cb231a0bf3644ec9a2b44c71f330
Buttreygoodness <jeff@readypress.com>
2013-06-19 14:17:55 -0700

    Added ability to create sections within chapters.

--- client/course.js
+++ client/course.js
@@ -1,6 +1,13 @@
 // course template
 
+Template.course.created = function () {
+  Session.set('selectedChapter', null);
+  Session.set('showCreateChapterDialog', null);
+  Session.set('showCreateSectionDialog', null);
+}
+
 Template.course.helpers({
+  
   course: function () {
     return Courses.findOne({_id: Session.get('currentCourse')});
   },
@@ -16,34 +23,63 @@ Template.course.helpers({
     });
   },
   
+  anySections: function () {
+    var secs = Modules.find({parentId: this._id});
+    return secs.count() > 0;
+  },
+  
+  sections: function () {
+    return Modules.find({
+      parentId: this._id
+    });
+  },
+  
   selectedChapter: function (i, e) {
     return this._id == Session.get('selectedChapter');
   },
   
   showCreateChapterDialog: function () {
     return Session.get('showCreateChapterDialog');
+  },
+  
+  showCreateSectionDialog: function () {
+    return Session.get('showCreateSectionDialog');
   }
+  
 });
 
 // courseControls template
 
 Template.courseControls.events({
+  
   'click .createChapter': function (event, template) {
     Session.set('showCreateChapterDialog', true);
+  },
+  
+  'click .createSection': function (event, template) {
+    Session.set('showCreateSectionDialog', true);
   }
+  
 });
 
 Template.course.events({
+  
   'click .chapter': function (event, template) {
     $('.selected').removeClass('selected');
     $(template.find('.' + this._id)).addClass('selected');
     Session.set('selectedChapter', this._id);
+  },
+  
+  'click .createSection': function (event, template) {
+    Session.set('showCreateSectionDialog', true);
   }
+  
 });
 
 // createChapterDialog template
 
 Template.createChapterDialog.events({
+  
   'click .cancel': function (event, template) {
     Session.set('showCreateChapterDialog', false);
   },
@@ -65,4 +101,34 @@ Template.createChapterDialog.events({
       });
     }
   }
+  
+});
+
+// createSectionDialog template
+
+Template.createSectionDialog.events({
+  
+  'click .cancel': function (event, template) {
+    Session.set('showCreateSectionDialog', false);
+  },
+  
+  'click .save': function (event, template) {
+    var title = template.find('.title').value;
+    var parent = Session.get('selectedChapter') || Session.get('currentCourse');
+    
+    if (title.length) {
+      Meteor.call('createSection', {
+        title: title,
+        parentId: parent
+      }, function (error, section){
+        if (! error) {
+          Session.set('selectedSection', section);
+          Session.set('showCreateSectionDialog', false);
+        } else {
+          console.log(error);
+        }
+      });
+    }
+  }
+  
 });
